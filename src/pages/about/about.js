ce256364391f4600d2e6bce89605a860220896d7
inier <inier@qq.com>
2020-11-26 19:42:33 +0800

    fix: 更新

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -4,9 +4,14 @@ import './about.scss';
 console.log('11111');
 
 import imgUrl from '../../images/cat.jpeg';
-
+import svgUrl from '../../images/female.svg';
 
 const img = document.createElement("img");
 img.style.width = "200px";
 img.src = imgUrl;
 document.body.appendChild(img);
+
+const svg = document.createElement("img");
+svg.style.width = "200px";
+svg.src = svgUrl;
+document.body.appendChild(svg);
